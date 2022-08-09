d327d14cdf6d27122c46783e7c8ae24ff789a9e2
Pascal Burkhard <pascal.burkhard@gmail.com>
2022-08-09 12:44:52 +0200

    Massive update

--- R/app_server.R
+++ R/app_server.R
@@ -6,6 +6,30 @@
 #' @import bs4Dash
 #' @noRd
 app_server <- function(input, output, session) {
+  # # Setup for SQLite DB
+  # credentials <- data.frame(
+  #   user = c("burkhardp"), # mandatory
+  #   password = c("planeyse"), # secret initial password
+  #   start = c("2022-01-01"), # optinal (all others)
+  #   expire = c(NA),
+  #   admin = c(TRUE),
+  #   stringsAsFactors = FALSE
+  # )
+  #
+  # keyring::key_set("fnchSpeaker", "dbAdmin") # Set password for SQLite DB
+  # shinymanager::create_db(
+  #   credentials_data = credentials,
+  #   sqlite_path = "inst/users.sqlite",
+  #   passphrase = keyring::key_get("fnchSpeaker", "dbAdmin")
+  # )
+
+  session$userData$auth <- shinymanager::secure_server(
+    check_credentials = shinymanager::check_credentials(
+      fs::path(rappdirs::user_data_dir(appname = "fnchSpeaker"), "users.sqlite"),
+      passphrase = keyring::key_get("fnchSpeaker", "dbAdmin")
+    )
+  )
+
   # Your application server logic
   mod_results_server("results")
 }
--- R/mod_results.R
+++ R/mod_results.R
@@ -25,7 +25,7 @@ mod_results_ui <- function(id){
             inputId = ns("event_id"),
             label = "Concours",
             choices = rsvps::get_fnch_sp_events() |>
-              dplyr::select(ort, id) |>
+              dplyr::select(titre, id) |>
               tibble::deframe()
           )
         ),
@@ -48,7 +48,23 @@ mod_results_ui <- function(id){
           shiny::numericInput(
             inputId = ns("nb_years"),
             label = "Nombre d'années",
-            value = 2, min = 2, max = 5, step = 1
+            value = 1, min = 1, max = 5, step = 1
+          )
+        )
+      ),
+      fluidRow(
+        col_6(
+          shiny::selectInput(
+            inputId = ns("class_min"),
+            label = "Catégorie min.",
+            choices = rsvps::get_fnch_sp_class_cat()
+          )
+        ),
+        col_6(
+          shiny::numericInput(
+            inputId = ns("titles_min"),
+            label = "Titres depuis",
+            value = 2015, min = 2015, max = format(Sys.Date(), "%Y") |> as.numeric() - 1, step = 1
           )
         )
       ),
@@ -74,7 +90,7 @@ mod_results_ui <- function(id){
         col_12(
           reactable::reactableOutput(
             outputId = ns("startlist_table")
-          )
+          ) |> shinycssloaders::withSpinner()
         )
       )
     )
@@ -96,7 +112,7 @@ mod_results_server <- function(id){
           inputId = ns("class_id"),
           label = "Épreuve",
           choices = rsvps::get_fnch_event_startlists(input$event_id) |>
-            dplyr::mutate(name = glue::glue("{nummer} - {name}"), id) |>
+            dplyr::mutate(name = glue::glue("{nummer} - {kategorie_text} - {name}"), id) |>
             dplyr::select(name, id) |>
             tibble::deframe()
         )
@@ -109,7 +125,9 @@ mod_results_server <- function(id){
           eventid = isolate(input$event_id),
           classid = isolate(input$class_id),
           nb_years = isolate(input$nb_years),
-          nb_ranks = isolate(input$nb_ranks)
+          nb_ranks = isolate(input$nb_ranks),
+          class_min = isolate(input$class_min),
+          titles_min = isolate(input$titles_min)
         )
       })
     })
